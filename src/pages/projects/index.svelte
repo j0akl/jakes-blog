38f4ba2678b3c4de3730f3e8856f66b0efdd0697
Jake Lynn <jklynn9@gmail.com>
2020-12-27 20:16:12 -0500

    added 10ser

--- src/pages/projects/index.svelte
+++ src/pages/projects/index.svelte
@@ -1,6 +1,8 @@
 
 <div id="container">
 	<h1>Projects</h1>
+	<a href="https://github.com/j0akl/10ser"><h2>Machine Learning from Scratch</h2></a>
+	<p>For this project, I am trying to make a deep learning library using only built in libraries (and numpy) in python. This will forever be a work in progress, at the moment I can train simple models with a couple different activation functions, as well as multiple layers. In the future, I want to bring the library up to date and include things like convolutional networks and optimizers.</p>
 	<a href="https://github.com/j0akl/speed-detection-3"><h2>Comma.ai Speed Challenge</h2></a>
 	<p>In this project, created as an interview challenge for Comma.ai, I was tasked with creating a machine learning algorithm that was given a dashcam video where every frame was labelled with the speed of the car at that moment, and to predict tthe speed of a car in another similar video.</p>
 	<p>This project taught me a ton about how to effectively manipulate data, especially when the correct use for that data is not immediately clear. I ended up using optical flow to get some meaningful data from the video. That ended up being a very productive rabbit hole in itself, and taught me how to pick up skills on the fly and implement them into a project. I also learned a lot about creating machine learning algorithms using PyTorch. This was the first project for which I used machine learning in a non-trivial capacity.</p>
