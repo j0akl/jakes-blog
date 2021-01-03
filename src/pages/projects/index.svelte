3833cf9ebdb72c8a05eb92f11212ad8cf3dd7320
Jake Lynn <jklynn9@gmail.com>
2021-01-03 14:37:19 -0500

    updated project page

--- src/pages/projects/index.svelte
+++ src/pages/projects/index.svelte
@@ -1,11 +1,15 @@
 
 <div id="container">
 	<h1>Projects</h1>
+	<a href="https://pdfhost.io/v/RYnUUUqKp_Proposalpdf.pdf"><h2>NASA Big Idea Proposal</h2></a>
+	<p>I wrote this paper with a group of other students from Northeastern proposing a pair of robots that would work together to explore craters on the moon that, due to the angle of the sun, permanetly exist in darkness. At the time of the paper's completion, I was the lead of the Doghouse team, and did much of the research, writing, and conceptualization in that section of the project. I also created filmed and edited the proposal video, which can be found on the Big Idea website. I have always loved the concept of space exploration, this project was a great way for me to get involved in an area I have been passionate about throughout my life. At the time of writing the paper, I was a physics major and was unable to contribute to the software component of the project. If I could go back and start over, I would have participated in the software development, which included localization, mapping, and wireless communication.</p>
 	<a href="https://github.com/j0akl/10ser"><h2>Machine Learning from Scratch</h2></a>
 	<p>For this project, I am trying to make a deep learning library using only built in libraries (and numpy) in python. This will forever be a work in progress, at the moment I can train simple models with a couple different activation functions, as well as multiple layers. In the future, I want to bring the library up to date and include things like convolutional networks and optimizers.</p>
 	<a href="https://github.com/j0akl/speed-detection-3"><h2>Comma.ai Speed Challenge</h2></a>
 	<p>In this project, created as an interview challenge for Comma.ai, I was tasked with creating a machine learning algorithm that was given a dashcam video where every frame was labelled with the speed of the car at that moment, and to predict tthe speed of a car in another similar video.</p>
 	<p>This project taught me a ton about how to effectively manipulate data, especially when the correct use for that data is not immediately clear. I ended up using optical flow to get some meaningful data from the video. That ended up being a very productive rabbit hole in itself, and taught me how to pick up skills on the fly and implement them into a project. I also learned a lot about creating machine learning algorithms using PyTorch. This was the first project for which I used machine learning in a non-trivial capacity.</p>
+	<a href="https://github.com/j0akl/chess"><h2>Chess Engine</h2></a>
+	<p>This project is a work in progess at the moment. My goal with the project was to teach the engine to play chess completely from scratch, giving it only the rules of the game and allowing it to play games against itself, akin to Google's AlphaZero. At the time of writing this description, I do not have access to enough compute power to simulate enough games in a reasonable time frame to train the model well enough to beat me, which was my end goal. I am currently rated around 950, so I think with enough optimization and training, the model will be able to beat me in the near future.</p>
 </div>
 
 <style>
